6b5de456886b90b6c7b8a0612bb9e113894608f0
Vicens Juan Tomas Monserrat <vicenc.monserrat@close.com>
2025-05-19 10:22:47 +0200

    Fix kill confirmation screen alignment

--- src/close_mongo_ops_manager/kill_confirmation_screen.py
+++ src/close_mongo_ops_manager/kill_confirmation_screen.py
@@ -15,8 +15,8 @@ class KillConfirmation(ModalScreen[bool]):
     DEFAULT_CSS = """
     KillConfirmation {
         align: center middle;
-        width: auto;
-        height: auto;
+        width: 100%;
+        height: 100%;
         padding: 0;
     }
 
@@ -24,10 +24,13 @@ class KillConfirmation(ModalScreen[bool]):
         background: $surface;
         border: thick $error;
         width: auto;
-        max-width: 50;
+        min-width: 40;
+        max-width: 60;
         height: auto;
+        min-height: 6;
         max-height: 10;
         padding: 1;
+        align: center middle;
     }
 
     #question {
@@ -40,10 +43,20 @@ class KillConfirmation(ModalScreen[bool]):
         width: 100%;
         align: center middle;
         padding-top: 1;
+        margin-top: 1;
     }
 
     Button {
         margin: 0 2;
+        min-width: 10;
+    }
+
+    #yes {
+        background: $error;
+    }
+
+    #no {
+        background: $primary;
     }
     """
 
@@ -64,7 +77,10 @@ class KillConfirmation(ModalScreen[bool]):
                 yield Button("No", variant="primary", id="no", classes="button")
 
     def on_mount(self) -> None:
-        self.query_one("#no")
+        """Set up the dialog when mounted."""
+        # Focus the No button by default (safer option)
+        no_button = self.query_one("#no", Button)
+        no_button.focus()
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         self.dismiss(event.button.id == "yes")
